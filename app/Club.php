0e076335d2fae593a2b4c0a5d97bb43a1e934c1b
Speakly <amaury.leproux@gmail.com>
2015-08-20 10:06:18 +0200

    merge

--- app/Club.php
+++ app/Club.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Club extends Model
+class Clubs extends Model
 {
     public function user()
     {
         return $this->hasMany('App\User');
+    }
+    public function game_team_id_2(){
+    	return $this->hasMany('App\Game', 'id', 'game_team_id_2');
     }
 }
